14cf112dd6841be3a96ca0b299f709e234db7255
Kendall-Cruz <kendallsoto51@gmail.com>
2025-09-15 23:05:13 -0600

    Diseño del scoreboard

--- app/(tabs)/ScoreBoard.tsx
+++ app/(tabs)/ScoreBoard.tsx
@@ -2,7 +2,7 @@ import { useCategoryStore } from '@/hooks/storage/useCategoryStore';
 import { View, Text, Dimensions, Image, Pressable, TouchableOpacity } from 'react-native';
 import Carousel from "react-native-reanimated-carousel";
 import Header from '@/components/Header';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { ICategory } from '@/interfaces/ICategory';
 import { IScoreBoardData } from '@/interfaces/IScoreBoardData';
 import { getTopTenUsers } from '@/services/UserScoreService';
@@ -14,6 +14,7 @@ const ScoreBoard = () => {
     const [level, setLevel] = useState<number | null>(null);
     const [topTen, setTopTen] = useState<IScoreBoardData[] | null>(null);
     const [currentCategory, setCurrentCategory] = useState<ICategory | null>(null);
+    const carouselRef = useRef<any>(null);
 
     const fetchTopTen = async (category: ICategory, level: number) => {
         setTopTen(null);
@@ -28,6 +29,10 @@ const ScoreBoard = () => {
     };
 
     useEffect(() => {
+        setLevel(1)
+    }, []);
+
+    useEffect(() => {
         if (level && currentCategory) {
             fetchTopTen(currentCategory, level);
         }
@@ -37,6 +42,7 @@ const ScoreBoard = () => {
         <View className="bg-[#1E293B]">
             <Header title="Puntuaciones" />
             <Carousel
+                ref={carouselRef}
                 width={Dimensions.get("window").width}
                 data={categories}
                 renderItem={({ item }) => (
@@ -52,39 +58,39 @@ const ScoreBoard = () => {
                         </View>
 
                         {/* Niveles */}
-                        <View className="flex-row justify-center bg-white/10 py-3 mx-5 rounded">
+                        <View className='flex-row justify-center bg-white rounded-sm py-4 mx-5 shadow-lg'>
                             <TouchableOpacity
-                                className={`border-b py-1 px-5 mx-2 ${level === 1 ? "bg-green-500/40" : "bg-black/20"
-                                    }`}
+                                className={`py-3 px-5 mx-2 rounded-sm ${level === 1 ? "bg-[#273A57]" : "bg-gray-300"}`}
                                 onPress={() => setLevel(1)}
                             >
-                                <Text className="text-white m-1 text-base">Fácil</Text>
+                                <Text className={`text-center font-semibold text-base ${level === 1 ? "text-white" : "text-gray-700"}`}>
+                                    Fácil
+                                </Text>
                             </TouchableOpacity>
 
-                            <View className="justify-center mx-1">
-                                <Text className="text-white">|</Text>
-                            </View>
+                            <View className="h-px bg-gray-200 mx-4 my-2" />
 
                             <TouchableOpacity
-                                className={`border-b py-1 px-5 mx-2 ${level === 2 ? "bg-green-500/40" : "bg-black/20"
-                                    }`}
+                                className={`py-3 px-5 mx-2 rounded-sm ${level === 2 ? "bg-[#273A57]" : "bg-gray-300"}`}
                                 onPress={() => setLevel(2)}
                             >
-                                <Text className="text-white m-1 text-base">Intermedio</Text>
+                                <Text className={`text-center font-semibold text-base ${level === 2 ? "text-white" : "text-gray-700"}`}>
+                                    Intermedio
+                                </Text>
                             </TouchableOpacity>
 
-                            <View className="justify-center mx-1">
-                                <Text className="text-white">|</Text>
-                            </View>
+                            <View className="h-px bg-gray-200 mx-4 my-2" />
 
                             <TouchableOpacity
-                                className={`border-b py-1 px-5 mx-2 ${level === 3 ? "bg-green-500/40" : "bg-black/20"
-                                    }`}
+                                className={`py-3 px-5 mx-2 rounded-sm ${level === 3 ? "bg-[#273A57]" : "bg-gray-300"}`}
                                 onPress={() => setLevel(3)}
                             >
-                                <Text className="text-white m-1 text-base">Difícil</Text>
+                                <Text className={`text-center font-semibold text-base ${level === 3 ? "text-white" : "text-gray-700"}`}>
+                                    Difícil
+                                </Text>
                             </TouchableOpacity>
                         </View>
+
 
                         {/* Tabla */}
                         <ScoreTable topTenList={topTen || []} />
@@ -97,56 +103,3 @@ const ScoreBoard = () => {
 };
 
 export default ScoreBoard
-/*
-const ScoreBoard = () => {
-   const { categories } = useCategoryStore();
-   const [level , setLevel] = useState();
-   const [topTen , setTopTen] = useState<IScoreBoardData[] | null>();
-
-   const onCategoryChange = async (category: ICategory) => {
-       setTopTen(null);
-       let topTenUsers = await getTopTenUsers(category._id , level!)
-       setTopTen(topTenUsers);
-   }
-
-   return (
-       <View className='bg-[#1E293B]'>
-           <Header title='Puntuaciones' />
-           <Carousel
-               width={Dimensions.get('window').width}
-               data={categories}
-               renderItem={({ item }) => (
-                   <>
-                       <View key={item.category} className="flex-row justify-center items-center text-center p-10">
-                           <Text className='text-2xl text-white text-center font-semibold'>{item.category}</Text>
-                           <Image source={{ uri: item.icon }} className='w-8 h-8 mx-1' />
-                       </View>
-                       <View className='flex-row justify-center bg-white/10 py-3 mx-5 rounded'>
-                           <TouchableOpacity className='border-b bg-black/20 py-1 px-5 mx-2' focusable={true}><Text className='text-white m-1 text-base'>Facíl</Text></TouchableOpacity>
-                           <View className="justify-center mx-1">
-                               <Text className="text-white">|</Text>
-                           </View>
-                           <TouchableOpacity className='border-b bg-black/20 py-1 px-5 mx-2'><Text className='text-white m-1 text-base'>Intermedio</Text></TouchableOpacity>
-                           <View className="justify-center mx-1">
-                               <Text className="text-white">|</Text>
-                           </View>
-                           <TouchableOpacity className='border-b bg-black/20 py-1 px-5 mx-2'><Text className='text-white m-1 text-base'>Dificíl</Text></TouchableOpacity>
-                       </View>
-                       <ScoreTable topTenList={topTen || []}></ScoreTable>
-                   </>
-
-
-
-               )}
-               onSnapToItem={(index) => {onCategoryChange(categories[index])}}
-           />
-
-
-
-       </View>
-   );
-};
-
-
-export default ScoreBoard;  */
-
--- components/ScoreTable.tsx
+++ components/ScoreTable.tsx
@@ -1,4 +1,4 @@
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, ScrollView } from "react-native";
 import { IScoreBoardData } from "@/interfaces/IScoreBoardData";
 
 interface Props {
@@ -7,34 +7,58 @@ interface Props {
 
 const ScoreTable = ({ topTenList }: Props) => {
     return (
-        <View className="bg-white/10 rounded mx-5 my-3">
-            <View className="flex-row border-b border-white/20 py-2 px-3">
-                <Text className="flex-1 text-white font-bold">Usuario</Text>
-                <Text className="w-20 text-white font-bold text-right">Score</Text>
+        <View className="bg-white rounded-sm mx-5 my-3 shadow-lg">
+            {/* Header */}
+            <View className="bg-gray-300 px-4 py-3">
+                <View className="flex-row items-center">
+                    <Text className="flex-1 text-gray-800 font-bold text-lg">Ranking</Text>
+                    <Text className="text-gray-800 font-bold">Puntuación</Text>
+                </View>
             </View>
+
             {topTenList.length !== 0 ? (
                 <FlatList
                     data={topTenList}
                     keyExtractor={(_, index) => index.toString()}
                     renderItem={({ item, index }) => (
-                        <View className="flex-row py-2 px-3 border-b border-white/10">
-                            <Text className="flex-1 text-white flex-row">
-                                <Text className={ index === 0 ? 'text-[#d4af37]' : index === 1 ? 'text-[#71d8e]' : index === 2 ? 'text-[#CD7F32]' : ''}>
+                        <View className={`flex-row items-center py-3 px-4 ${index !== topTenList.length - 1 ? 'border-b border-gray-200' : ''}`}>
+                            {/* Posición */}
+                            <View className="w-8 h-8 rounded-full bg-blue-100 items-center justify-center mr-3">
+                                <Text className={`font-bold ${
+                                    index === 0 ? 'text-[#e6c403]' : 
+                                    index === 1 ? 'text-[#6a83a8]' : 
+                                    index === 2 ? 'text-[#CD7F32]' : 
+                                    'text-gray-700'
+                                }`}>
                                     {index + 1}
                                 </Text>
-                                {" - "}{item.username}
+                            </View>
+
+                            {/* Nombre usuario */}
+                            <Text className="flex-1 text-gray-800 text-base">
+                                {item.username}
                             </Text>
-                            <Text className="w-20 text-white text-right">{item.maxScore}</Text>
+
+                            {/* Score */}
+                            <View className="bg-blue-50 rounded-lg px-3 py-1">
+                                <Text className="text-blue-700 font-semibold text-right">
+                                    {item.maxScore}
+                                </Text>
+                            </View>
                         </View>
                     )}
                 />
             ) : (
-                <View className="m-5">
-                    <Text className="text-center text-base text-white">No hay puntuaciones disponibles de momento</Text>
+                <View className="py-8 px-4">
+                    <View className="items-center">
+                        <Text className="text-center text-base text-gray-600">
+                            No hay puntuaciones disponibles de momento
+                        </Text>
+                    </View>
                 </View>
             )}
         </View>
     );
 };
 
-export default ScoreTable;
+export default ScoreTable
